0424e66e92616d09f3639dea390efaed5b4ed1fb
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-02-14 08:15:34 -0500

    Updated from Brython Server: 2/14/2018 8:15:33 AM

--- loopDemo.py
+++ loopDemo.py
@@ -1,1 +1,12 @@
+#Daniel Bandler
+#2/14/18
+#loopDemo.py demonstrates for and while loops
 
+"""
+#I love computer science 5 times
+for i in range(1,6):
+    print("I <3 Computer science")
+"""
+
+for i in range(1,11):
+    print(i)
