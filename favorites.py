6741582c766cccdfbcf76f7fcf6dfc05112993da
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-02-14 08:34:53 -0500

    Updated from Brython Server: 2/14/2018 8:34:52 AM

--- favorites.py
+++ favorites.py
@@ -1,1 +1,10 @@
+#Daniel Bandler
+#2/14/18
+#favorites.py prints word specified number of times
 
+word = input("WHat is your favorite word?")
+num = int(input("What is your favorite number?"))
+
+
+for i in range(1,num+1):
+    print(word)
