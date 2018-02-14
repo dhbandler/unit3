420dd0506d930e7600848139f9032ff3dcd5d726
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-02-14 08:30:46 -0500

    Updated from Brython Server: 2/14/2018 8:30:45 AM

--- loopDemo.py
+++ loopDemo.py
@@ -7,6 +7,54 @@
 for i in range(1,6):
     print("I <3 Computer science")
 """
+"""
+#while loop version
 
+i = 0
+while i < 5:
+   print("I <3 Computer science") 
+   i = i+1 #update step
+
+"""
+
+
+"""
+#counts from 1 to 10
 for i in range(1,11):
     print(i)
+"""
+"""
+i=1
+while i <=10:
+    print(i)
+    i += 1 #adds one to i
+    
+ """   
+#Count from 27 to 43 by 2's
+
+"""
+for i in range(27,44,2): #start, stop (+1) and how much it goes up by
+    print(i)
+"""
+i = 27
+while i < 44:
+    print(i)
+    1 += 2
+
+"""
+#add up the numbers from 1-5
+total = 0
+for i in range(1,6):
+    total = total + i
+    
+print(total)
+"""
+"""
+total = 0
+i = 1
+while i <=5:
+    total = tpta; + 1
+    i += 1
+print(total)
+
+"""
