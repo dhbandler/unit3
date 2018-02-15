4903dd6361ac12ab4630bdec3f5e89cd794ea71d
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-02-15 08:38:28 -0500

    Updated from Brython Server: 2/15/2018 8:38:27 AM

--- gauss.py
+++ gauss.py
@@ -1,9 +1,11 @@
 #Daniel Bandler
 #2/15/18
 #gauss.py adds up numbers from 1-100
-
+num1 = int(input("What is the first number? "))
+num2 = int(input("What is the second number? "))
+dif = int(input("What number do you want to count by? "))
 total = 0
-for i in range(1,101):
+for i in range(num1,num2+1,dif):
     total = total + i
     
 print(total)
