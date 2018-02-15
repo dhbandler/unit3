f26f3708df4efbf1b8b0d3fe6216af8ea1ea931d
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-02-15 08:43:40 -0500

    Updated from Brython Server: 2/15/2018 8:43:40 AM

--- hitchhiker.py
+++ hitchhiker.py
@@ -1,1 +1,10 @@
-
+#Daniel Bandler
+#2/15/18
+#hitchhiker.py responds to questions
+i = 0
+while i == 0:
+    inp1 = input("Ask a question or type 'quit' ")
+    if inp1 == "quit":
+        break
+    else:
+        print("42")
