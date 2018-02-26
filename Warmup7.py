4fffdd67aac4c95ffecfa477f7e19ceab733ec33
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-02-26 08:12:51 -0500

    Updated from Brython Server: 2/26/2018 8:12:50 AM

--- Warmup7.py
+++ Warmup7.py
@@ -1,1 +1,13 @@
+#Daniel Bandler
+#2/26/18
+#Warmup7.py - use a loop to print the product of the numbers from 1 to 20
+total = 1
+for num in range(1,21):
+    total = total*num
+    num=num+1
+
+    
+print(total, "is your answer")
+    
+    
 
