4bf01aa001041eb7875c5bbd53f5a0788e4e54ec
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-02-26 08:42:15 -0500

    Updated from Brython Server: 2/26/2018 8:42:10 AM

--- divisors.py
+++ divisors.py
@@ -1,1 +1,9 @@
+#Daniel Bandler
+#2/26/18
+#divisors.py finds round divisors of number
+num = int(input("Input a number here "))
+for i in range(1,num//2+1):
+    if num % i == 0:
+        print(i)
 
+    
