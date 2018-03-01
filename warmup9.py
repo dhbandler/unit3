515330a58ed8509fd6629e0eedc4d9fbb794f971
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-03-01 08:24:20 -0500

    Updated from Brython Server: 3/1/2018 8:24:19 AM

--- warmup9.py
+++ warmup9.py
@@ -1,1 +1,14 @@
+#Daniel Bandler
+#3/1/18
+#warmup9.py - capitalizes vowels with loops
 
+text = input("Type something")
+
+
+
+for ch in text:
+    if ch == "a" or ch == "e" or ch == "i" or ch == "o" or ch == "u" :
+        print(ch.upper())
+        
+    else:
+        print(ch)
