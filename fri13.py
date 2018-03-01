b74faa88f469608a3c0d828b7ffd08f6babff0b6
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-03-01 08:48:20 -0500

    Updated from Brython Server: 3/1/2018 8:48:19 AM

--- fri13.py
+++ fri13.py
@@ -1,1 +1,19 @@
+#Daniel Bandler
+#3/1/18
+#fri13.py - lists all friday the 13ths
 
+from datetime import *
+from calendar import weekday
+
+
+
+"""
+while date.today().day >= 13:
+    print(date.today())
+    date.today().day += 1
+    if date.today().day > 13:
+        date.today().day -= 13
+        date.today().month += 1
+    if friday == weekday(date.today().year, date.today().month, date.today().day):
+        print(date)
+"""
