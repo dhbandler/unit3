331740907dd7093dd3857dfef096f06fa7600486
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-03-02 08:48:51 -0500

    Updated from Brython Server: 3/2/2018 8:48:49 AM

--- wallpaper.py
+++ wallpaper.py
@@ -1,1 +1,21 @@
+#Daniel Bandler
+#3/2/18
+#wallpaper.py makes a wallpaper
 
+
+from ggame import *
+
+
+black = Color(0x000000, 1)
+red = Color(0xff0000, 1)
+blue = Colot(0x0000ff, 1)
+white = Color(0xffffff, 1)
+
+redRectangle = RectangleAsset(1.14, .0769, blackOutline, red)
+
+
+for i in range(12):   #putting a row of dots
+    for j in range(12):
+       Sprite(dot, (10+(radius*2+10)*i,10+(radius*2+10)*j)) 
+       
+App().run()
